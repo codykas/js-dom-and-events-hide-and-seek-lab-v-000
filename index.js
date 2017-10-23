09d78c772bc03384c1951fe1495f5c36d6da6324
Cody Kasselman <ckasselman@fordham.edu>
2017-10-23 21:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,11 +21,7 @@ function deepestChild(){
 function increaseRankBy(n){
   const rankedLists = document.querySelectorAll('.ranked-list')
 
-  for (let i = 0, l = rankedLists.length; i < l; i++){
-    let children = rankedLists[i].children
-
-    for (let j = 0, k = children.length; j < k; j++){
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
-    }
+  for (let i = 0, l = rankedList.length; i < l; i++){
+    rankedList[i].innerHTML = parseInt(rankedList[i]) + n
   }
 }
